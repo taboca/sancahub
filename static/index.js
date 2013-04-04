217548b482aa0e7d39fbba5dff7f9d90a26c5ff2
marcio <mgalli@telasocial.com>
2013-04-04 14:37:47 -0300

    Galery

--- static/index.js
+++ static/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
 register("/main/qqqqqqqqqqqqqqqq", "0.698045977995738", "./header/index.html", iframeTemplate);
-register("/main/wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww", "destaques", "./destaques/index.html", iframeTemplate);
-register("/main/eeeeeeeeeeeeeeeeeeeeeeee", "0.7342910511830653", "./noticias/index.html", iframeTemplate);
+register("/main/wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww", "destaques", "./galeria/index.html", iframeTemplate);
 register("/main/ttttttttttttttt", "0.8841019960292364", "./news/index.html", iframeTemplate);
 register("/main/yyyyyyyyy", "0.8448067060305994", "./weather/index.html", iframeTemplate);
 register("/main/uuuuuuuuuuuuuuuu", "0.13553831341819889", "./twitter-new/index.html", iframeTemplate);
